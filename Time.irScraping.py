7790e1625f090d4c615c6a2b490405e24a94eb6a
Home PC <barazesh.mr90@gmail.com>
2021-05-05 17:42:19 +0430

    get full date in getYear

--- Time.irScraping.py
+++ Time.irScraping.py
@@ -26,25 +26,76 @@ def getYear(year):
     driver.find_element_by_id(
         'ctl00_cphTop_Sampa_Web_View_EventUI_EventYearCalendar10cphTop_3417_btnGo').click()
     page = driver.page_source
-    soup=BS(page,'lxml')
-    months = soup.select('#ctl00_cphTop_Sampa_Web_View_EventUI_EventYearCalendar10cphTop_3417_pnlYearCalendar .panel.panel-body')
-    monthCounter = 0
-    for m in months:
-        monthCounter += 1
+    soup = BS(page, 'lxml')
+    monthsHTML = soup.select(
+        '#ctl00_cphTop_Sampa_Web_View_EventUI_EventYearCalendar10cphTop_3417_pnlYearCalendar .panel.panel-body')
+    shamsiMonth = 0
+    for m in monthsHTML:
+        daysList = []
+        shamsiMonth += 1
         print('************')
-        print(f'month: {monthCounter}')
-        days = m.select('.eventCalendar .mainCalendar .dayList > div')
+        curr_months = m.select('.eventCalendar .header .dates > span')[0]
+        miladi_months = __getMiladiMonths(
+            curr_months.select('.miladi')[0].string)
+        qamari_months = __getQamariMonths(
+            curr_months.select('.qamari')[0].string)
+        print(
+            f'Shamsi month: {shamsiMonth}--Miladi months:{miladi_months}--Qamari months:{qamari_months}')
+        daysHTML = m.select('.eventCalendar .mainCalendar .dayList > div')
         print('###')
-        for d in days:
-            isDisabled='disabled' in d.attrs['class']
+        miladiMonthChanged = False
+        qamariMonthChanged = False
+        qamariMonthChangedAgain=False
+        for d in daysHTML:
+            isDisabled = 'disabled' in d.attrs['class']
             if 'disabled' not in d.attrs['class']:
                 if d.select('.holiday'):
                     print('*Holiday*')
-                jalali=d.select('.jalali')[0].string
-                miladi=d.select('.miladi')[0].string
-                qamari=d.select('.qamari')[0].string
-                print(f'Jalali: {jalali} -- Miladi: {miladi} -- Qamari: {qamari}')
-                print('-----------------')
+                jalali = d.select('.jalali')[0].string
+                jalaliDay = f'{year}/{shamsiMonth}/{jalali}'
+                miladi = d.select('.miladi')[0].string
+                if miladi == '1':
+                    miladiMonthChanged = True
+                if miladiMonthChanged:
+                    miladiMonth = miladi_months[1]
+                else:
+                    miladiMonth = miladi_months[0]
+
+                miladiDay = f'{miladiMonth}/{miladi}'
+                qamari = d.select('.qamari')[0].string
+                if qamari == '1' and not qamariMonthChanged:
+                    qamariMonthChanged = True
+                elif qamari == '1' and  qamariMonthChanged:
+                    qamariMonthChangedAgain=True
+
+                if qamariMonthChanged and not qamariMonthChangedAgain:
+                    qamariMonth = qamari_months[1]
+                elif qamariMonthChangedAgain and qamariMonthChanged:
+                    qamariMonth = qamari_months[2]
+                else:
+                    qamariMonth = qamari_months[0]
+                qamariDay = f'{qamariMonth}/{qamari}'
+                # print(f'Jalali: {jalali} -- Miladi: {miladi} -- Qamari: {qamari}')
+                # print('-----------------')
+
+
+def __getMiladiMonths(input):
+    monthNames=['January','February','March','April','May','June','July','August','September','October','November','December']
+    months=input.split('-')
+    secondMonthTemp=months[1].strip().split(' ')
+    year=secondMonthTemp[1]
+    secondMonth=f'{year}/{monthNames.index(secondMonthTemp[0])+1}'
+    if any(char.isdigit() for char in months[0]):
+        firstmonthTemp=months[0].strip().split(' ')
+        firstmonth=f'{firstmonthTemp[1]}/{monthNames.index(firstmonthTemp[0])+1}'
+    else:
+        firstmonth=f'{year}/{monthNames.index(months[0].strip())+1}'
+
+    return (firstmonth,secondMonth)
+
+
+def __getQamariMonths(input):
+    pass
 
 
 def getDay(inputDate):
